51a8cad4ea077ac5944a437b7ebbc765d7a6dc77
unknown <mihai.badi@yahoo.com>
2021-10-16 17:45:15 +0300

    Task #1

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,16 +2,25 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener{
     private EditText eName;
-    private TextView tName;
     private Button bName;
+    private Spinner spin;
+    private final String[] colors = {"White", "Red", "Green", "Blue", "Yellow"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +29,53 @@ public class MainActivity extends AppCompatActivity {
 
         eName = (EditText) findViewById(R.id.eName);
         bName = (Button) findViewById(R.id.bClick);
-        tName = (TextView) findViewById(R.id.tName);
+        spin = (Spinner) findViewById(R.id.spinner);
+
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, colors);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         bName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tName.setText("Hello, " + eName.getText().toString());
+                //tName.setText("Hello, " + eName.getText().toString());
+
+                builder.setTitle(R.string.dialog_title)
+                        .setMessage("Hello, " + eName.getText().toString())
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(getApplicationContext(),"you choose yes action for alertbox",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel,  new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                AlertDialog alert = builder.create();
+                alert.show();
             }
         });
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), colors[position] , Toast.LENGTH_LONG).show();
+        bName.setTextColor(Color.parseColor(colors[position]));
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        bName.setTextColor(Color.parseColor("#FFFFFF"));
     }
 }
